b143836653efbe46000fe7bb9ca72e994e9f96be
Jakub Margielewski <kuba.margielewski@gmail.com>
2017-07-13 15:10:14 +0200

    Basic structure without yepnope

--- js/game.js
+++ js/game.js
@@ -1,11 +1,27 @@
+import {BubbleShoot} from "./ui.js";
+
+$(function() {
+
 var BubbleShoot = window.BubbleShoot || {};
+
 BubbleShoot.Game = (function($){
     var Game = function(){
         this.init = function(){
-            $(".but_start_game").bind("click", stratGame);
+            $(".but_start_game").bind("click", startGame);
         };
         var startGame = function(){
+            console.log("hello");
+            $(".but_start_game").unbind("click");
+            // BubbleShoot.ui.hideDialog();
+            $(".dialog").fadeOut(300);
         };
     };
     return Game;
 })(jQuery);
+
+var game = new BubbleShoot.Game();
+game.init();
+
+});
+
+console.log(BubbleShoot);
---
+++ js/ui.js
@@ -0,0 +1,14 @@
+var BubbleShoot = window.BubbleShoot || {};
+
+BubbleShoot.ui = (function($){
+    var ui = {
+        init : function(){
+        },
+        hideDialog : function(){
+            $(".dialog").fadeOut(300);
+        }
+    };
+    return ui;
+});
+
+export {BubbleShoot};
